4f414edde5c683c8d0dd523915f25a6970f33a0b
Alban de Crevoisier <albandecrevoisier@gmail.com>
2021-12-05 18:38:25 +0100

    Correct alignment in day 1 source.

--- 2021/1.scm
+++ 2021/1.scm
@@ -9,17 +9,17 @@
 |#
 
 (define (input->list port)
- (let ((line (read-line port)))
-   (if (eof-object? line)
-       '()
-       (cons (string->number line) (input->list port)))))
+  (let ((line (read-line port)))
+    (if (eof-object? line)
+        '()
+        (cons (string->number line) (input->list port)))))
 
 (define (number-times-increases depths)
   (if (< (length depths) 2)
       0
       (+ (if (< (car depths) (cadr depths))
-              1
-              0)
+             1
+             0)
          (number-times-increases (cdr depths)))))
 
 ; Call on input
