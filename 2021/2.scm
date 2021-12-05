43d27d97f7b8ec331da248de88c6e9cda42375a3
Alban de Crevoisier <albandecrevoisier@gmail.com>
2021-12-05 19:40:24 +0100

    Day 2 part 2.

--- 2021/2.scm
+++ 2021/2.scm
@@ -32,3 +32,40 @@
 (display ((lambda (p) (* (car p) (cdr p)))
             (move-sub (open-input-file "2") 0 0)))
 (newline)
+
+
+#|
+  New interpretation of the instructions:
+  - down X increases your aim by X units.
+  - up X decreases your aim by X units.
+  - forward X does two things:
+    - It increases your horizontal position by X units.
+    - It increases your depth by your aim multiplied by X.
+  2/ Same question with the new interpretation.
+|#
+
+(define (move-sub-with-aim port x y aim)
+  (let ((line (read-line port)))
+    (if (eof-object? line)
+        (cons x y)
+        (cond
+          ((char-whitespace? (string-ref line 2)) ; "up X"
+           (move-sub-with-aim port
+                              x
+                              y
+                              (- aim (string->number (substring line 3)))))
+          ((char-whitespace? (string-ref line 4)) ; "down X"
+           (move-sub-with-aim port
+                              x
+                              y
+                              (+ aim (string->number (substring line 5)))))
+          ((char-whitespace? (string-ref line 7)) ; "forward X"
+           (move-sub-with-aim port
+                              (+ x (string->number (substring line 8)))
+                              (+ y (* aim (string->number (substring line 8))))
+                              aim))))))
+
+; Call on input
+(display ((lambda (p) (* (car p) (cdr p)))
+            (move-sub-with-aim (open-input-file "2") 0 0 0)))
+(newline)
