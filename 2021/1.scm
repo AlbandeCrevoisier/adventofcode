cb1f30a3ee2c768325cfeb90654ecbb29725a97c
Alban de Crevoisier <albandecrevoisier@gmail.com>
2021-12-05 17:56:17 +0100

    day 1 part 2

--- 2021/1.scm
+++ 2021/1.scm
@@ -1,3 +1,13 @@
+#|
+  Example input:
+  199
+  200
+  208
+  210
+  These represent successive depths.
+  1/ How many times does the depth increase?
+|#
+
 (define (input->list port)
  (let ((line (read-line port)))
    (if (eof-object? line)
@@ -12,5 +22,19 @@
               0)
          (number-times-increases (cdr depths)))))
 
-;; Call on input
+; Call on input
 (display (number-times-increases (call-with-input-file "1" input->list)))
+(newline)
+
+; 2/ Same question but by using a 3-values sliding window.
+
+(define (depths->windows depths)
+  (if (< (length depths) 3)
+      '()
+      (cons (+ (car depths) (cadr depths) (caddr depths))
+            (depths->windows (cdr depths)))))
+
+; Call on input
+(display (number-times-increases
+           (depths->windows (call-with-input-file "1" input->list))))
+(newline)
