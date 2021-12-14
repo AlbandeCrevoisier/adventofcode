cf24ad541a9913de254248c9fcf2769e629605e1
Alban de Crevoisier <albandecrevoisier@gmail.com>
2021-12-14 23:04:38 +0100

    Day 4 part 1

---
+++ 2021/4.scm
@@ -0,0 +1,121 @@
+#|
+  Game of bingo.
+  1/ Sum of all unmarked numbers of the winning card multiplyed
+  by the last drawn number.
+  input example:
+  1,2,3
+
+  1 2
+  3 4
+
+  5 6
+  7 8
+  Notes:
+  - There is a space in front of single digit numbers to maintain
+  alignment with double digits ones.
+  - Grids are 5 by 5, with numbers in [0, 99].
+  - Hypothesis: numbers can only appear once on a card.
+  - Let's use complex number to denote crossed numbers.
+|#
+
+(define (read-draws port)
+  (let ((c (read-char port)))
+    (if (char=? c #\newline)
+        '()
+        (let ((cc (read-char port)))
+          (cond ((char=? cc #\newline)
+                 (list (digit-value c)))
+                ((char=? cc #\,)
+                 (cons (digit-value c) (read-draws port)))
+                (else
+                 (if (char=? #\, (peek-char port))
+                     (read-char port)) ; read the #\, out of the port
+                 (cons (+ (* 10 (digit-value c)) (digit-value cc))
+                       (read-draws port))))))))
+
+(define (read-card-number port)
+  (let ((c (read-char port)))
+    (let ((cc (read-char port)))
+      (if (char=? c #\space)
+          (digit-value cc)
+          (+ (* 10 (digit-value c)) (digit-value cc))))))
+
+(define (read-card-line port)
+  (let ((n (read-card-number port)))
+    (if (char=? (read-char port) #\space)
+        (cons n (read-card-line port))
+        (list n))))
+
+(define (read-card port)
+  (let ((l (read-card-line port)))
+    (if (or (eof-object? (peek-char port))
+            (char=? (peek-char port) #\newline))
+        (list l)
+        (cons l (read-card port)))))
+
+(define (read-cards port)
+  (if (eof-object? (read-char port))
+      '()
+      (let ((card (read-card port)))
+        (cons card (read-cards port)))))
+
+(define (remove-non-reals row)
+  (if (null? row)
+      '()
+      (let ((c (car row)))
+        (cons (if (= (imag-part c) 0) c 0)
+              (remove-non-reals (cdr row))))))
+
+(define (sum-row card)
+  (map (lambda (x) (apply + x)) card))
+
+(define (sum-col card)
+  (if (null? (car card))
+      '()
+      (cons (apply + (map car card))
+            (sum-col (map cdr card)))))
+
+(define (card-sum card)
+  (apply + (sum-row (map remove-non-reals card))))
+
+(define (card-won? card)
+  (= (length card)
+     (max (apply max (map imag-part (sum-row card)))
+          (apply max (map imag-part (sum-col card))))))
+
+(define (mark-draw-in-row draw row)
+  (cond ((null? row) '())
+        ((= draw (car row))
+         (cons (+ draw 1i) (mark-draw-in-row draw (cdr row))))
+        (else (cons (car row) (mark-draw-in-row draw (cdr row))))))
+
+(define (mark-draw draw card)
+  (map (lambda (x) (mark-draw-in-row draw x)) card))
+
+(define (check-cards draw cards)
+  (if (null? cards)
+      '()
+      (let ((c (mark-draw draw (car cards))))
+        (if (card-won? c)
+            (list (* draw (card-sum c)))
+            (cons c (check-cards draw (cdr cards)))))))
+
+(define (has-won? cards)
+  (cond ((number? cards) cards) ; the first card won
+        ((number? (car (reverse cards)))
+         (car (reverse cards))); ugly, but...
+        (else #f))) ; numbers count as #t, only #f counts as #f.
+
+(define (play-bingo draws cards)
+  (let ((marked (check-cards (car draws) cards)))
+    (let ((res (has-won? marked)))
+      (if res
+          res
+          (play-bingo (cdr draws) marked)))))
+
+(define (play-bingo-with-input port)
+  (let ((d (read-draws port)))
+    (play-bingo d (read-cards port))))
+
+(define (day4-part1)
+  (call-with-input-file "4" play-bingo-with-input))
