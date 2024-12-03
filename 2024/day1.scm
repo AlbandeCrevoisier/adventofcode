3ea17fe6072a9f3c199de2ef75283b112b928186
∇ <albandecrevoisier@gmail.com>
2024-12-03 15:23:14 +0100

    day 1 (part 2)

--- 2024/day1.scm
+++ 2024/day1.scm
@@ -8,8 +8,9 @@
 ;; 1   3
 ;; 3   9
 ;; 3   3
-;; Which would yield: [1 2 3 3 3 4] & [3 3 3 4 5 9],
+;; would yield: [1 2 3 3 3 4] & [3 3 3 4 5 9],
 ;; then 2 + 1 + 0 + 1 + 2 + 5 = 11.
+
 ;; The problem seems rather easy, though parsing might be a chore.
 (import (scheme base))  ;; R7RS
 (import (chibi)) ;; IO
@@ -21,21 +22,68 @@
     (if (eof-object? rl)
         '()
         (cons rl (read-all-lines f)))))
+;;(define input-list (call-with-input-file "day1-example" read-all-lines))
 (define input-list (call-with-input-file "day1-input" read-all-lines))
 
 (define (extract-numeric s) (regexp-extract '(+ numeric) s))
 (define extracted-input (map extract-numeric input-list))
 
-(define left-input (map car extracted-input))
-(define right-input (map cadr extracted-input))
+(define left-input (map string->number (map car extracted-input)))
+(define right-input (map string->number (map cadr extracted-input)))
 
-(define (parse-sort-input l)
-  (list-sort < (map string->number l)))
-(define left-sorted (parse-sort-input left-input))
-(define right-sorted (parse-sort-input right-input))
+(define left-sorted (list-sort < left-input))
+(define right-sorted (list-sort < right-input))
 
 (define (difference a b) (abs (- a b)))
 (define (sum-differences ls rs)
   (apply + (map difference ls rs)))
 
 (display (sum-differences left-sorted right-sorted))
+(display "\n")
+
+
+;; The second part of the problem is to compute a similarity score
+;; between each list: we multiply each number in the left column by
+;; the number of times it appears in the right one.
+;; Example:
+;; 3   4
+;; 4   3
+;; 2   5
+;; 1   3
+;; 3   9
+;; 3   3
+;; would yield: 3*3 + 4*1 + 2*0 + 1*0 + 3*3 + 3*3 = 31.
+;; It quickly appears that this is, for each number in the left
+;; column, the sum of number * n_left * n_right:
+;; 3 3 3
+;; 4 1 1
+;; 2 1 0
+;; 1 1 0
+;; 5 0 1
+;; 9 0 1
+;; => 3*3*3 + 4*1*1 + 2*1*0 + 1*1*0 = 31
+;; Therefore, it would probably be useful to transform our lists to
+;; count each value, & to store that in a structure that makes it easy
+;; to access an arbitrary value: a hash table (I miss my python data
+;; tools, where this would have been a trivial group by & inner join!)
+
+(import (srfi 69)) ;; hash tables
+
+(define (input->hash-table input-list)
+  (let ((input-ht (make-hash-table)))
+    (let ((insert-value ;; updates value counts
+           (lambda (val)
+             (hash-table-update!/default input-ht
+                                         val
+                                         (lambda (x) (+ x 1)) 0))))
+      (for-each insert-value input-list)
+      input-ht)))
+
+(define left-ht (input->hash-table left-input))
+(define right-ht (input->hash-table right-input))
+
+(define (update-similarity-score val count score)
+  (+ score
+     (* val count (hash-table-ref/default right-ht val 0))))
+(display (hash-table-fold left-ht update-similarity-score 0))
+(display "\n")
