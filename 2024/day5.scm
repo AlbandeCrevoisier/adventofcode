736e490f9ef761ea4bc650ac5d6135238589c1b8
∇ <albandecrevoisier@gmail.com>
2025-01-09 21:32:06 +0100

    day5 WIP

--- 2024/day5.scm
+++ 2024/day5.scm
@@ -3,6 +3,9 @@
 
 ;; 75,47,61,53,29
 ;; 97,61,53,29,13
+
+;; My plan is to build a table containing the page dependencies:
+;; for a|c, a|b, have a: b, c .
 (import (scheme base)
         (utils)
         (srfi 1) ;; list processing
@@ -17,5 +20,16 @@
 (define orderings (take lines line-break))
 (define manuals (drop lines (+ 1 line-break)))
 
-(define t (make-hash-table (make-default-comparator)))
+(define (parse-orderings orderings) (map extract-numeric orderings))
+(define (parse-manuals manuals) (map extract-numeric manuals))
 
+(define dependencies (make-hash-table (make-default-comparator)))
+(define (insert-dependency ordering)
+  (let ((updater (lambda (deps) (cons (car ordering) deps))))
+    (hash-table-update!/default dependencies
+                                (cadr ordering)
+                                updater
+                                '())))
+(define (insert-dependencies)
+  (map insert-dependency (parse-orderings orderings)))
+(insert-dependencies)
