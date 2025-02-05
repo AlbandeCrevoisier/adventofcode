9b1016b2c7e62a60a5be397f18aa148ccee5b079
∇ <albandecrevoisier@gmail.com>
2025-02-05 22:34:03 +0100

    day 5 part 2

--- 2024/day5.scm
+++ 2024/day5.scm
@@ -17,13 +17,15 @@
 (define lines (read-input-file "day5-input"))
 
 (define (string-empty? s) (string=? s ""))
-(define line-break (list-index string-empty? lines))
-
-(define orderings (take lines line-break))
-(define manuals (drop lines (+ 1 line-break)))
+(define (find-line-break lines) (list-index string-empty? lines))
 
 (define (parse-orderings orderings) (map extract-numeric orderings))
+(define (get-orderings lines)
+  (parse-orderings (take lines (find-line-break lines))))
+
 (define (parse-manuals manuals) (map extract-numeric manuals))
+(define (get-manuals lines)
+  (parse-manuals (drop lines (+ (find-line-break lines) 1))))
 
 (define dependencies (make-hash-table (make-default-comparator)))
 (define (insert-dependency ordering)
@@ -33,22 +35,23 @@
                                 updater
                                 '())))
 (define (insert-dependencies)
-  (map insert-dependency (parse-orderings orderings)))
+  (map insert-dependency (get-orderings lines)))
 (insert-dependencies)
 
 (define (get-dependencies page) (hash-table-ref/default dependencies
                                                         page
                                                         '()))
 
-(define (valid-page? page following-pages dependencies)
-  (iset-empty? (iset-intersection (list->iset following-pages)
-                                  (list->iset dependencies))))
+(define (valid-page? page other-pages)
+  (let ((page-dependencies (get-dependencies page)))
+    (iset-empty? (iset-intersection (list->iset other-pages)
+                                    (list->iset page-dependencies)))))
 (define (valid-manual? manual)
   (let ((page (car manual))
         (following-pages (cdr manual)))
     (if (<= (length manual) 1)
         #t
-        (and (valid-page? page following-pages (get-dependencies page))
+        (and (valid-page? page following-pages)
              (valid-manual? following-pages)))))
 
 (define (filter-valid-manuals manuals)
@@ -61,6 +64,32 @@
 (define (sum-middle-pages manuals)
   (apply + (middle-pages manuals)))
 
-(define (sum-valid-manuals-mid-pages raw-manuals)
-  (sum-middle-pages (filter-valid-manuals (parse-manuals raw-manuals))))
-(displayln (sum-valid-manuals-mid-pages manuals))
+(define (sum-valid-manuals-mid-pages manuals)
+  (sum-middle-pages (filter-valid-manuals manuals)))
+(displayln (sum-valid-manuals-mid-pages (get-manuals lines)))
+
+
+;; Part 2 requires that we filter out the valid manuals, correct the invalid
+;; ones, & sum their middle pages.
+
+;; -> iteratively pick the page that is in conflict with none other, then
+;; recurse on the list with this page removed
+;; Needs something like "pick the first element in that list that satisfies
+;; this predicate -> (any , I think? => (find , actually, or (list-index
+;; Also: return the list with this value dropped
+;; => use (find & (delete
+
+(define (remove-valid-manuals manuals)
+  (remove valid-manual? manuals))
+
+(define (order manual)
+  (if (<= (length manual) 1)
+      manual
+      (let ((first-page? (lambda (page) (valid-page? page manual))))
+        (let ((first-page (find first-page? manual)))
+          (cons first-page (order (delete first-page manual)))))))
+(define (order-manuals manuals) (map order manuals))
+
+(define (sum-invalid-manuals-mid-pages manuals)
+  (sum-middle-pages (order-manuals (remove-valid-manuals manuals))))
+(displayln (sum-invalid-manuals-mid-pages (get-manuals lines)))
